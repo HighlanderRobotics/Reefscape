24a0e9abed5c17c50aa6f10b436fdb4ca6f44dcd
SCool62 <samthecool300@icloud.com>
2025-01-15 20:35:35 -0800

    Begin work on Intake subsystem

--- src/main/java/frc/robot/subsystems/intake/IntakePivotIOReal.java
+++ src/main/java/frc/robot/subsystems/intake/IntakePivotIOReal.java
@@ -11,43 +11,38 @@ import edu.wpi.first.units.measure.Current;
 import edu.wpi.first.units.measure.Temperature;
 
 public class IntakePivotIOReal implements IntakePivotIO {
-    // TODO: SET DEVICE ID
-    private final TalonFX pivotMotor = new TalonFX(0, "*");
+  // TODO: SET DEVICE ID
+  private final TalonFX pivotMotor = new TalonFX(0, "*");
 
-    private final StatusSignal<AngularVelocity> angularVelocityRotsPerSec = pivotMotor.getVelocity();
-    private final StatusSignal<Temperature> temp = pivotMotor.getDeviceTemp();
-    private final StatusSignal<Current> supplyCurrentAmps = pivotMotor.getSupplyCurrent();
-    private final StatusSignal<Current> currentAmps = pivotMotor.getStatorCurrent();
-    private final StatusSignal<Angle> motorPositionRotations = pivotMotor.getPosition();
+  private final StatusSignal<AngularVelocity> angularVelocityRotsPerSec = pivotMotor.getVelocity();
+  private final StatusSignal<Temperature> temp = pivotMotor.getDeviceTemp();
+  private final StatusSignal<Current> supplyCurrentAmps = pivotMotor.getSupplyCurrent();
+  private final StatusSignal<Current> currentAmps = pivotMotor.getStatorCurrent();
+  private final StatusSignal<Angle> motorPositionRotations = pivotMotor.getPosition();
 
-    public IntakePivotIOReal() {
-        // TODO: MOTOR CONFIGURATION
-    }
+  public IntakePivotIOReal() {
+    // TODO: MOTOR CONFIGURATION
+  }
 
-    @Override
-    public void updateInputs(IntakePivotIOInputs inputs) {
-        BaseStatusSignal.refreshAll(
-                angularVelocityRotsPerSec,
-                temp,
-                supplyCurrentAmps,
-                currentAmps,
-                motorPositionRotations
-        );
+  @Override
+  public void updateInputs(IntakePivotIOInputs inputs) {
+    BaseStatusSignal.refreshAll(
+        angularVelocityRotsPerSec, temp, supplyCurrentAmps, currentAmps, motorPositionRotations);
 
-        inputs.angularVelocityRotsPerSec = angularVelocityRotsPerSec.getValueAsDouble();
-        inputs.tempDegreesC = temp.getValue().in(Units.Celsius);
-        inputs.supplyCurrentAmps = supplyCurrentAmps.getValueAsDouble();
-        inputs.currentAmps = currentAmps.getValueAsDouble();
-        inputs.pivotPosition = Rotation2d.fromRotations(motorPositionRotations.getValueAsDouble());
-    }
+    inputs.angularVelocityRotsPerSec = angularVelocityRotsPerSec.getValueAsDouble();
+    inputs.tempDegreesC = temp.getValue().in(Units.Celsius);
+    inputs.supplyCurrentAmps = supplyCurrentAmps.getValueAsDouble();
+    inputs.currentAmps = currentAmps.getValueAsDouble();
+    inputs.pivotPosition = Rotation2d.fromRotations(motorPositionRotations.getValueAsDouble());
+  }
 
-    @Override
-    public void setMotorVoltage(double voltage) {
-        pivotMotor.setVoltage(voltage);
-    }
+  @Override
+  public void setMotorVoltage(double voltage) {
+    pivotMotor.setVoltage(voltage);
+  }
 
-    @Override
-    public void setMotorPosition(Rotation2d targetPosition) {
-        pivotMotor.setPosition(targetPosition.getRotations());
-    }
+  @Override
+  public void setMotorPosition(Rotation2d targetPosition) {
+    pivotMotor.setPosition(targetPosition.getRotations());
+  }
 }
---
+++ src/main/java/frc/robot/subsystems/intake/IntakePivotSubsystem.java
@@ -0,0 +1,15 @@
+package frc.robot.subsystems.intake;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import org.littletonrobotics.junction.Logger;
+
+public class IntakePivotSubsystem extends SubsystemBase {
+  private IntakePivotIO io;
+  private IntakePivotIOInputsAutoLogged inputs;
+
+  @Override
+  public void periodic() {
+      io.updateInputs(inputs);
+      Logger.processInputs("Intake/Pivot", inputs);
+  }
+}
