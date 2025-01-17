c76f34f95fbb0090975af6d3a211297125b5424f
SCool62 <79726681+SCool62@users.noreply.github.com>
2025-01-17 02:19:19 +0000

    run spotless

--- src/main/java/frc/robot/subsystems/intake/IntakePivotIOSim.java
+++ src/main/java/frc/robot/subsystems/intake/IntakePivotIOSim.java
@@ -1,16 +1,15 @@
 package frc.robot.subsystems.intake;
 
+import static edu.wpi.first.units.Units.RadiansPerSecond;
+import static edu.wpi.first.units.Units.RotationsPerSecond;
+
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.controller.ArmFeedforward;
 import edu.wpi.first.math.controller.ProfiledPIDController;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
-import edu.wpi.first.units.Units;
 import edu.wpi.first.wpilibj.simulation.SingleJointedArmSim;
-
-import static edu.wpi.first.units.Units.RadiansPerSecond;
-import static edu.wpi.first.units.Units.RotationsPerSecond;
 
 public class IntakePivotIOSim implements IntakePivotIO {
   // TODO: SET TO ACTUAL VALUES WHEN CAD IS FINISHED
@@ -24,11 +23,11 @@ public class IntakePivotIOSim implements IntakePivotIO {
           0.0,
           0.5,
           true,
-          0.0
-      );
+          0.0);
 
   // TODO: TUNE
-  ProfiledPIDController pivotPid = new ProfiledPIDController(0.0, 0.0, 0.0, new TrapezoidProfile.Constraints(10.0, 10.0));
+  ProfiledPIDController pivotPid =
+      new ProfiledPIDController(0.0, 0.0, 0.0, new TrapezoidProfile.Constraints(10.0, 10.0));
   // TODO: TUNE
   ArmFeedforward pivotFf = new ArmFeedforward(0.0, 0.0, 0.0);
 
@@ -37,7 +36,8 @@ public class IntakePivotIOSim implements IntakePivotIO {
     intakePivotSim.update(0.02);
 
     inputs.pivotPosition = Rotation2d.fromRadians(intakePivotSim.getAngleRads());
-    inputs.angularVelocityRotsPerSec = RadiansPerSecond.of(intakePivotSim.getVelocityRadPerSec()).in(RotationsPerSecond);
+    inputs.angularVelocityRotsPerSec =
+        RadiansPerSecond.of(intakePivotSim.getVelocityRadPerSec()).in(RotationsPerSecond);
     inputs.currentAmps = intakePivotSim.getCurrentDrawAmps();
     inputs.supplyCurrentAmps = 0.0;
     inputs.tempDegreesC = 0.0;
@@ -50,7 +50,8 @@ public class IntakePivotIOSim implements IntakePivotIO {
 
   @Override
   public void setMotorPosition(Rotation2d targetPosition) {
-    setMotorVoltage(pivotPid.calculate(intakePivotSim.getAngleRads(), targetPosition.getRadians())
-        + pivotFf.calculate(pivotPid.getSetpoint().position, pivotPid.getSetpoint().velocity));
+    setMotorVoltage(
+        pivotPid.calculate(intakePivotSim.getAngleRads(), targetPosition.getRadians())
+            + pivotFf.calculate(pivotPid.getSetpoint().position, pivotPid.getSetpoint().velocity));
   }
 }
