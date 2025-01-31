8c21372ebd09c15a639a8ea5431edaab925973be
Lewis-Seiden <lewy@seiden.us>
2025-01-30 17:11:27 -0800

    make model sort of work

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -499,9 +499,25 @@ public class Robot extends LoggedRobot {
     Logger.recordOutput(
         "Mechanism Poses",
         new Pose3d[] {
-          new Pose3d(
+          new Pose3d( // first stage
               new Translation3d(0, 0, elevator.getExtensionMeters() / 2.0), new Rotation3d()),
-          new Pose3d(new Translation3d(0, 0, elevator.getExtensionMeters()), new Rotation3d())
+          // carriage
+          new Pose3d(new Translation3d(0, 0, elevator.getExtensionMeters()), new Rotation3d()),
+          new Pose3d( // arm
+              new Translation3d(
+                  ShoulderSubsystem.X_OFFSET_METERS,
+                  0,
+                  elevator.getExtensionMeters() + ShoulderSubsystem.Z_OFFSET_METERS),
+              new Rotation3d(0, (shoulder.getAngle().getRadians() - (Math.PI / 2.0)), 0)),
+          new Pose3d( // Manipulator
+              new Translation3d(
+                  ShoulderSubsystem.X_OFFSET_METERS
+                      - shoulder.getAngle().getCos() * ShoulderSubsystem.ARM_LENGTH_METERS,
+                  0,
+                  elevator.getExtensionMeters()
+                      + ShoulderSubsystem.Z_OFFSET_METERS
+                      + shoulder.getAngle().getSin() * ShoulderSubsystem.ARM_LENGTH_METERS),
+              new Rotation3d(0, wrist.getAngle().getRadians(), 0))
         });
     Logger.recordOutput("AutoAim/Target", AutoAimTargets.getClosestTarget(swerve.getPose()));
 
