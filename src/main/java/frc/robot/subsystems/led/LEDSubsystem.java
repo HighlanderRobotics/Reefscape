4f11648ac690bd0eb76bba41c2c778d5f98989d7
Lewis-Seiden <lewy@seiden.us>
2025-02-10 19:31:49 -0800

    fix package declarations

--- src/main/java/frc/robot/subsystems/led/LEDSubsystem.java
+++ src/main/java/frc/robot/subsystems/led/LEDSubsystem.java
@@ -2,7 +2,7 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.subsystems.leds;
+package frc.robot.subsystems.led;
 
 import com.google.common.base.Supplier;
 import edu.wpi.first.wpilibj.DriverStation;
