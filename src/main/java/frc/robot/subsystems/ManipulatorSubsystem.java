6a8bb29598342ce3af8fcba27f6b75bb20c562e2
petropapahadjopoulos <anotherguywithabeanie@gmail.com>
2025-03-13 16:29:10 -0700

    finish merge

--- src/main/java/frc/robot/subsystems/ManipulatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/ManipulatorSubsystem.java
@@ -12,7 +12,7 @@ import edu.wpi.first.math.filter.LinearFilter;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
-import frc.robot.subsystems.arm.ShoulderSubsystem;
+import frc.robot.subsystems.shoulder.ShoulderSubsystem;
 import frc.robot.subsystems.wrist.WristSubsystem;
 import frc.robot.Robot;
 import frc.robot.Robot.RobotType;
