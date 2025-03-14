e873144db4241b5b142c0432e2374f1b876068a3
Lewis-Seiden <lewy@seiden.us>
2025-03-14 16:01:44 -0700

    further tuning seems to work

--- src/main/java/frc/robot/subsystems/Superstructure.java
+++ src/main/java/frc/robot/subsystems/Superstructure.java
@@ -3,6 +3,7 @@ package frc.robot.subsystems;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -175,9 +176,15 @@ public class Superstructure {
         .get(SuperState.IDLE)
         .whileTrue(
             extendWithClearance(
-                ElevatorSubsystem.HP_EXTENSION_METERS,
-                ShoulderSubsystem.SHOULDER_HP_POS,
-                WristSubsystem.WRIST_HP_POS))
+                    Units.inchesToMeters(5.0),
+                    ShoulderSubsystem.SHOULDER_HP_POS,
+                    WristSubsystem.WRIST_HP_POS)
+                .until(() -> elevator.isNearExtension(Units.inchesToMeters(5.0)))
+                .andThen(
+                    extendWithClearance(
+                        ElevatorSubsystem.HP_EXTENSION_METERS,
+                        ShoulderSubsystem.SHOULDER_HP_POS,
+                        WristSubsystem.WRIST_HP_POS)))
         .whileTrue(manipulator.index())
         .whileTrue(
             funnel.setVoltage(
