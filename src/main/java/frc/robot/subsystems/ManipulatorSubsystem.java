dc69d5eb76336244a22b2211e7cd86b55607a7e1
Lewis-Seiden <lewy@seiden.us>
2025-04-05 15:09:12 -0700

    slow down reverse indexing

--- src/main/java/frc/robot/subsystems/ManipulatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/ManipulatorSubsystem.java
@@ -152,7 +152,7 @@ public class ManipulatorSubsystem extends RollerSubsystem {
                   io.setPosition(Rotation2d.fromRotations(0.63));
                   positionSetpoint = 0.63;
                 }),
-        setVoltage(0.25).until(() -> !firstBBInputs.get),
+        setVoltage(0.1).until(() -> !firstBBInputs.get),
         jog(CORAL_HOLD_POS).until(() -> !secondBBInputs.get && !firstBBInputs.get));
   }
 
